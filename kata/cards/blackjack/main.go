df76fa7735a832f327613e745cb8d397dd7e9bd4
Zack Patrick <zpatrick319@Hotmail.com>
2018-05-10 17:56:53 -0700

    df

---
+++ kata/cards/blackjack/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+
+func RuneToASCII(r rune) string {
+    if r < 128 {
+        return string(r)
+    } else {
+        return "\\u" + strconv.FormatInt(int64(r), 16)
+    }
+}
+
+func main() {
+	thing := '♠'
+	fmt.Printf("%q", thing)
+}
